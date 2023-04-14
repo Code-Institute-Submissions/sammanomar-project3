8a6f75aca5931413ffb69946aff2074c5954d4d5
sammanomar <>
2023-04-14 16:52:03 +0000

    install numpy and create connect four board matrix

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# install numpy to build up the matrix
+# create board (6 rows by 7 columns)
+import numpy as np
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+board = create_board()
+print(board)
