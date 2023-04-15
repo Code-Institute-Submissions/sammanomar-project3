0905614b835845b093b4601829ecc91cd90a90c7
sammanomar <>
2023-04-15 14:42:46 +0000

    flip board build up orientation form bottom to up at numpy

--- run.py
+++ run.py
@@ -1,23 +1,32 @@
 # install numpy
 import numpy as np
 
+ROW_COUNT = 6       #global variable
+COLUMN_COUNT = 7    #global variable
+
 # create board (6 rows by 7 columns matrix)
 def create_board():
     board = np.zeros((6,7))
     return board
 
 # drop piece
-def drop_piece():
-    pass
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
 
 def is_valid_location(board, col):
     return board [5][col] == 0
 
-def get_next_open_row():
-    pass
+def get_next_open_row(board, col):    # which row piece will fall on
+    for r in range(ROW_COUNT):
+        if board [r][col] == 0:
+            return r
+
+def print_board(board):     #flip the board build up orientation from bottom to up at numpy
+    print(np.flip(board,0))
+    
 
 board = create_board()
-print(board)   
+print_board(board)   
 game_over = False
 turn = 0    #differnitate between player 1 and player 2 turn
 
@@ -27,9 +36,20 @@ while not game_over:
     if turn == 0:
         col = int(input("Player 1 Make your Selection (0-6):"))
 
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+
+
     # ask for player 2 input
     else: 
         col = int(input("Player 2 Make your Selection (0-6):"))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+    print_board(board)        
 
     # switching between player 1 and player 2
     turn += 1
