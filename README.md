3084c80da54061f0c27448b92c54fbcd1e8c4664
sammanomar <sammanomar@hotmail.com>
2023-04-21 18:46:15 +0000

    fix pep8 bugs, update screenshots and read me

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CONNECT FOUR GAME
 Connect four game  is a Python terminal game. which runs in the Code Institute mock terminal on Heroku. 2 Users (X or O) can try to beat each other by dropping and connecting four pieces whether vertically or horizontally or diagonally. The first player to connect four will win.
 
-Here Is the live version of my project
+Here Is the live version of my project:
 
 ![](/screenshots/a.png)
 
@@ -14,66 +14,74 @@ The winner is the player who connects four four first whether vertically or hori
 
 ## Features
 ### Existing Features
-- Random 6 row X 8 column board generation
+- Flexible 6 row X 8 column board generation
 - Marked up board with columns number on the top
 
 ![](/screenshots/b.png)
 
-- Play against another player
+- Play against another player without computer
 - Accept users input
 ![](/screenshots/c.png)
 ![](/screenshots/d.png)
 
 - Input validation and error-checking
-1. You cannot enter strings or texts
+1. You cannot enter string
 ![](/screenshots/e.png)
-2. You must enter numbers from 0 to 8 only
+2. You must enter numbers from 1 to 8 only
 ![](/screenshots/f.png)
-3. Zero, negative values, letter or any special character values are not acceptable
+3. Zero, negative values, or any special character values are not acceptable
 ![](/screenshots/i.png)
-
+4. Game saves the last move of any player on the top of the title of the game
+![](/screenshots/j.png)
 - data maintained in class instances
+5. Win and draw messages
+![](/screenshots/h.png)
+![](/screenshots/k.png)
 
 ### Future Features
 - Allow more players to play the game
-- bigger size board with more players 
+- Add difficulty levels
+- Add player VS computer option
 
 ## Data Model 
 I decided to use a Board class as my model. The game creates the instance of the Board class. 
 The Board class prints the slots of the board, rotate the turn of the player, check the position of the dropped piece. check the possibility of win against that board in all directions.
-I used a python trick to print 64 dashes all next to each other using `print` method.
+I used a python trick to print 50 dashes all next to each other using `print` method.
 
 ## Testing
-[ have manually tested this proiect by doing the following:
+Have manually tested this proiect by doing the following:
 - passed the code through a PE8 linter and confirmed there are no problems
-- Given invalid inouts: strings when numbers are expected out of bounds inputs.
+- Given invalid inputs: strings when numbers are expected out of bounds inputs.
 - Tested in my local terminal and the Code Institute Heroku terminal
 
 ## Bugs 
-When I wrote the proiect. I was aetting index errors because I had forgotten that the lists are zero indexed. I fixed
-this br addina size _ 1 where necessary
-properly
+- When I wrote the project. I had an issue with validating zero and negative entries. I fixed
+this by using if conditional statment
+- E501 error line too long by PEP8. I fixed it using line continuation operator \ and parenthesis()
 
 ## Remaining Bugs
-• No bugs remaining
+No bugs remaining
 
 ## Validator Testing
 - PEP8
 No errors were returned from PEP8.online.com
+![](/screenshots/l.png)
 
 ## Deployment
 This proiect was deployed using Code Institute's mock terminal for Heroku.
 
-Stens for deplovment
-o Fork or clone this renocitor.
-a Crosto a nom Horobu onn
-o link the Herokuann to the renociton
-click on Donlou
+- Steps for deployment
+1. Fork or clone this repository
+2. Create a new Heroku app
+3. Set the buildpacks to Python and NodeJS in that order
+4. Link the Herokuapp to the repository
+5. Click on Deploy
 
-Credits
+## Credits
 - Code Institute for the deployment terminal
 - Wikipedia for the details of the connect four game
 - My mentor Lauren Nicole
 - freeCodeCamp.org
 - stackoverflow.com
 - quora.com
+- doingmathwithpython.github.io
--- run.py
+++ run.py
@@ -1,26 +1,30 @@
 ROW_COUNT = 6      # global variable
 COLUMN_COUNT = 8   # global variable
 
+
 # create board object (8 rows by 12 columns)
 class Board():
+
     # store state of the game and handle users input until there is a winner
     def __init__(self):
         """
         Instance variable
         Create board array
         """
-        self.board = [[' ' for _ in range(COLUMN_COUNT)] for _ in range(ROW_COUNT)]
-        self.turns = 0 # track turns that have been played and last move input
-        self.last_move = [-1, -1] # [r, c] # [r, c] (invalid move because we haven't played anything yet)
-
+        self.board = \
+            [[' ' for _ in range(COLUMN_COUNT)] for _ in range(ROW_COUNT)]
+        # track turns that have been played and last move input
+        self.turns = 0
+        # [r, c] invalid move because we haven't played anything yet
+        self.last_move = [-1, -1]
 
     def print_board(self):
         """
-        Reusable function to display the board 
+        Reusable function to display the board
         to the terminal as the game goes on
         """
         # number the columns
-        print("\n") # new line
+        print("\n")  # new line
         # display what number this column is and start counting at 1
         for r in range(COLUMN_COUNT):
             print(f"  ({r+1}) ", end="")
@@ -32,10 +36,9 @@ class Board():
             for c in range(COLUMN_COUNT):
                 print(f"  {self.board[r][c]}  |", end="")
             print("\n")
-        # python trick to print 50 dashes all next to each other  
+        # python trick to print 50 dashes all next to each other
         print(f"{'-' * 50}\n")
 
-    
     def which_turn(self):
         """
         Switching between player X and player O
@@ -43,14 +46,12 @@ class Board():
         players = ['X', 'O']
         return players[self.turns % 2]
 
-    
     def in_bounds(self, r, c):
         """
         Test row column coordinates is within our 2D array
         """
         return (r >= 0 and r < ROW_COUNT and c >= 0 and c < COLUMN_COUNT)
 
-    
     def turn(self, column):
         """
         Turn function
@@ -58,40 +59,41 @@ class Board():
         # Search bottom up for an open slot
         for i in range(ROW_COUNT-1, -1, -1):
             if self.board[i][column] == ' ':
-                self.board[i][column] = self.which_turn() # execute this turn and put X or O here
-                self.last_move = [i, column] # save this last move to the spot
+                # execute this turn and put X or O here
+                self.board[i][column] = self.which_turn()
+                self.last_move = [i, column]  # save this last move to the spot
 
-                self.turns += 1 # increment the turn that has been played
-                return True # to indicate that was a valid move
+                self.turns += 1  # increment the turn that has been played
+                return True  # to indicate that was a valid move
 
         return False
-
 
     def check_winner(self):
         """
         Check winner function
         """
-        last_row = self.last_move[0] # tracking
-        last_col = self.last_move[1] # tracking
-        last_letter = self.board[last_row][last_col] # tracking
+        last_row = self.last_move[0]  # tracking
+        last_col = self.last_move[1]  # tracking
+        last_letter = self.board[last_row][last_col]  # tracking
 
         # [r, c] direction, matching letter count, locked bool
-        directions = [[[-1, 0], 0, True], # check upward
+        directions = [[[-1, 0], 0, True],  # check upward
                       [[1, 0], 0, True],  # check upward
-                      [[0, -1], 0, True], # check downard
+                      [[0, -1], 0, True],  # check downard
                       [[0, 1], 0, True],  # check downard
-                      [[-1, -1], 0, True],# check left
+                      [[-1, -1], 0, True],  # check left
                       [[1, 1], 0, True],  # check right
-                      [[-1, 1], 0, True], # check diagonally
-                      [[1, -1], 0, True]] # check diagonally
-        
+                      [[-1, 1], 0, True],  # check diagonally
+                      [[1, -1], 0, True]]  # check diagonally
+
         # Search outwards looking for matching pieces
         for a in range(4):
             for d in directions:
                 r = last_row + (d[0][0] * (a+1))
                 c = last_col + (d[0][1] * (a+1))
 
-                if d[2] and self.in_bounds(r, c) and self.board[r][c] == last_letter:
+                if (d[2] and self.in_bounds(r, c) and
+                        self.board[r][c] == last_letter):
                     d[1] += 1
                 else:
                     # Stop searching in this direction
@@ -102,7 +104,7 @@ class Board():
             if (directions[i][1] + directions[i+1][1] >= 3):
                 self.print_board()
                 print(f"{last_letter} is the winner!")
-                return last_letter   
+                return last_letter
 
         # Did not find any winners
         return False
@@ -122,29 +124,28 @@ def play():
         print("--------------------------------------------------")
         game.print_board()
 
-
         # Ask the user for input, but only accept valid turns
         valid_move = False
         while not valid_move:
             try:
-                user_move = int(input(f"{game.which_turn()}'s Turn - pick a column (1-{COLUMN_COUNT}): "))
-                if user_move <= 0 or user_move > 8:
+                user_move = int(input(f"{game.which_turn()}'s Turn - \
+pick a column (1-{COLUMN_COUNT}): "))
+                if user_move <= 0 or user_move > (COLUMN_COUNT):
                     print(f"Please choose a number from 1 and {COLUMN_COUNT}")
-                else:    
-                    valid_move = (game.turn(int(user_move)-1))    
+                else:
+                    valid_move = (game.turn(int(user_move)-1))
             except ValueError:
                 print(f"Please choose a number from 1 and {COLUMN_COUNT}")
 
-                            
         # End the game if there is a winner
         game_over = game.check_winner()
-        
         # End the game if there is a tie
         if not any(' ' in x for x in game.board):
             print("The game is a draw..")
             return
 
-#set up game loop with python trick that execute play function when file is run
+
+"""set up game loop with python trick that
+executes play function when file is run"""
 if __name__ == '__main__':
     play()
-
